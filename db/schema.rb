b595284ec2313c65a35b215e38bead3c7d60f581
Ubuntu <ubuntu@ip-172-31-55-185.ec2.internal>
2022-11-09 09:01:10 +0000

    user作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_11_09_085144) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "uid"
+    t.string "pass"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

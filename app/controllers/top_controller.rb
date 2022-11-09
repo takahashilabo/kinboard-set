9771a64935b206bf7e4496b10948692dbe0cd18e
Ubuntu <ubuntu@ip-172-31-55-185.ec2.internal>
2022-11-09 07:59:09 +0000

    top修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,30 +1,5 @@
 class TopsController < ApplicationController
   def main
-    if session[:uid]
-      render 'main'
-    else
-      render 'login'
-    end
-  end
-  
-  def login
-    user = User.find_by(uid: params[:uid])
-    if user
-      login_password = BCrypt::Password.new(user.pass)
-      if login_password == params[:pass]
-        session[:uid] = user.uid
-        redirect_to tops_main_path
-      else
-        render 'login'
-      end
-    else
-      render 'login'
-    end
-  end
-    
-  def logout
-      session.delete(:uid)
-      redirect_to tops_main_path
   end
     
 end

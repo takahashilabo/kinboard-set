5e7be6c01761a624e6ef1618d5a54570ac47839a
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-11-16 07:05:07 +0000

    profileページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:index, :new, :create, :edit, :update, :destroy]
   resources :scores, only: [:index]
   get 'top/main'
   root 'top#main'
+  get 'users/profile'
 end

13549b4ce73c9db522c5c70a1649a6ea89755f90
2011140037 <ubuntu@ip-172-31-55-185.ec2.internal>
2022-11-30 08:35:27 +0000

    set image

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -9,7 +9,10 @@ class ResponsesController < ApplicationController
   end
   
   def create
-    @response = Response.new(message: params[:response][:message], user_id: params[:response][:user_id], thre_id: params[:response][:thre_id], tdate: Time.current, image: params[:response][:image])
+    @response = Response.new(message: params[:response][:message], user_id: params[:response][:user_id], thre_id: params[:response][:thre_id], tdate: Time.current)
+    if image = params[:response][:image]
+      @response.image.attach(image)
+    end
     if @response.save
       redirect_to responses_path #"/**"
     else
@@ -21,5 +24,9 @@ class ResponsesController < ApplicationController
     response = Response.find(params[:id])
     response.destroy
     redirect_to responses_path
+  end
+  
+  def show
+    @response = Response.find(params[:id])
   end
 end
